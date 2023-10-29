db24f5e5e0d2ca13883f91341e3651eef3667e79
Kaj Berg <kajberg84@gmail.com>
2023-10-29 18:06:03 +0100

    add login router

--- routes/login-router.js
+++ routes/login-router.js
@@ -1,12 +1,11 @@
 import express from 'express'
-
+import { LoginController } from '../controller/login-controller.js'
 export const loginRouter = express.Router()
 
+const controller = new LoginController()
 //localhost:8080/login
 loginRouter.get('/', (req, res, next) => {
     res.send("Loginrouter WORK")
 })
 
-loginRouter.post('/', (req, res, next) => {
-    res.send("post works, login")
-})
+loginRouter.post('/', (req, res, next) => controller.login(req, res, next))
